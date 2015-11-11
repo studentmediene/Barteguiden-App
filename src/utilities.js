b9b2c23a2baf4f6d352abc2a042a65a0c00565fa
Anna Kastet <annakastet@gmail.com>
2015-11-11 22:51:28 +0100

    Add getTimeFromDate function

--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,10 @@
 'use strict';
+import moment from 'moment';
 
 export function sortByDate (event1, event2) {
   return new Date(event1.startAt).getTime() - new Date(event2.startAt).getTime();
+};
+
+export function getTimeFromDate(date) {
+  return moment(date).format('HH:mm');
 };
