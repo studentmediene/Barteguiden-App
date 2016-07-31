5605aa457973fce13dea31aa4731563fd3990912
Anna Kastet <annakastet@gmail.com>
2016-07-31 14:34:53 +0200

    Remove Map-, Share-, and CalendarButton
    Move Calendar and Share functionality to ActionToolbar. Present map functionality as an External Link.
    
    Resolves #67

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -5,17 +5,17 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {formatDate, formatPrice} from '../utilities';
 import EventDescription from './EventDescription';
 import EventDetailsImage from './EventDetailsImage';
-import {backgroundColor} from '../colors';
+import ActionToolbar from './ActionToolbar';
+import ExternalLink from './ExternalLink';
+import {backgroundColor, highlightColor, containerColor, separatorColor} from '../colors';
 
 import {
   StyleSheet,
-  Text,
   View,
   Image,
-  TouchableOpacity,
+  Linking,
   ScrollView,
   Platform,
 } from 'react-native';
@@ -26,13 +26,42 @@ class EventDetails extends Component {
     return (
       <View style={[styles.container, {paddingBottom: bottomPadding}]}>
         <EventDetailsImage event={this.props.event}/>
+        <ActionToolbar event={this.props.event} />
         <ScrollView style={styles.scroll}>
-          <View style={styles.descriptionContainer}>
-            <EventDescription event={this.props.event}/>
+          <EventDescription event={this.props.event}/>
+          <View style={styles.externalLinkContainer}>
+            <ExternalLink linkStyle={styles.externalLink}
+              containerStyle={[styles.externalLinkRow, styles.bottomBorder]}
+              showIcon={true} url={this.props.event.eventUrl}
+              linkText={'Besøk nettsiden'} iconStyle={styles.linkIconStyle}/>
+            <ExternalLink linkStyle={styles.externalLink}
+              containerStyle={styles.externalLinkRow} showIcon={true}
+              onPress={this.onMapClick.bind(this)} linkText={'Åpne i kart'}
+              iconStyle={styles.linkIconStyle}/>
           </View>
         </ScrollView>
       </View>
     );
+  }
+
+  onMapClick() {
+    let {latitude, longitude, name} = this.props.event.venue;
+    let url;
+
+    if (Platform.OS === 'android') {
+      url = 'geo:0,0?q=' + latitude + ',' + longitude + '(' + name + ')';
+    }
+    else {
+      url = `http://maps.apple.com/?q=${name.split(' ').join('+')}&sll=${latitude},${longitude}&z=10`
+    }
+
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log('Don\'t know how to open URI: ' + url);
+      }
+    });
   }
 }
 
@@ -41,13 +70,31 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
-  descriptionContainer: {
-    backgroundColor: backgroundColor,
+  externalLink: {
+    fontSize: 16,
+    flexDirection: 'row',
     flex: 1,
+    color: 'black',
+  },
+  externalLinkRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  bottomBorder: {
+    borderColor: separatorColor,
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
   scroll: {
     backgroundColor: backgroundColor,
-  }
+  },
+  externalLinkContainer: {
+    flexDirection: 'column',
+    backgroundColor: containerColor,
+    borderColor: separatorColor,
+    borderWidth: StyleSheet.hairlineWidth,
+  },
 });
 
 export default EventDetails;
