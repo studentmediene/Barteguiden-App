064a654c53f21f8d17f7c32e8a822d744bb1d01b
Anna Kastet <annakastet@gmail.com>
2016-08-12 22:38:31 +0200

    Hide PromotedEventSwiper if there are no promoted events
    Resolves #31

--- src/components/PromotedEventSwiper.js
+++ src/components/PromotedEventSwiper.js
@@ -9,31 +9,36 @@ const height = Dimensions.get('window').height / 3;
 const PromotedEventSwiper = (props) => {
   const promotedEvents = props.events.filter(event => event.isPromoted).slice(0, 10);
   return (
-    <Swiper
-      height={height}
-      autoplay
-      autoplayTimeout={5}
-      dot={
-        <View
-          style={{ backgroundColor: 'rgba(255,255,255,.5)', width: 3, height: 3,
-            borderRadius: 4, marginLeft: 3, marginRight: 3,
-          marginTop: 3, marginBottom: 3, bottom: -22 }}
-        />}
-      activeDot={
-        <View
-          style={{ backgroundColor: '#fff', width: 6, height: 6,
-            borderRadius: 4, marginLeft: 3, marginRight: 3,
-          marginTop: 3, marginBottom: 3, bottom: -22 }}
-        />}
-    >
-      {promotedEvents.map(promoted => (
-        <PromotedEvent
-          navigator={props.navigator} key={promoted._id}
-          promoted={promoted}
-        />
-        ))
+    <View>
+      {promotedEvents.length > 0 ?
+        <Swiper
+          height={height}
+          autoplay
+          autoplayTimeout={5}
+          dot={
+            <View
+              style={{ backgroundColor: 'rgba(255,255,255,.5)', width: 3, height: 3,
+                borderRadius: 4, marginLeft: 3, marginRight: 3,
+              marginTop: 3, marginBottom: 3, bottom: -22 }}
+            />}
+          activeDot={
+            <View
+              style={{ backgroundColor: '#fff', width: 6, height: 6,
+                borderRadius: 4, marginLeft: 3, marginRight: 3,
+              marginTop: 3, marginBottom: 3, bottom: -22 }}
+            />}
+        >
+          {promotedEvents.map(promoted => (
+            <PromotedEvent
+              navigator={props.navigator} key={promoted._id}
+              promoted={promoted}
+            />
+            ))
+          }
+        </Swiper>
+        : null
       }
-    </Swiper>
+    </View>
     );
 };
 
